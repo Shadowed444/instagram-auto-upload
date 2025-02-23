3ed250c83a903db70270747dade502efbcd798c5
Shadowed444 <prakul.maddi@gmail.com>
2025-02-23 23:39:50 +0530

    Updated auto_upload.py to start automation scripts

--- auto_upload.py
+++ auto_upload.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,5 +9,10 @@ def home():
     return "Instagram Auto Uploader is Running!"
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))  # Uses the PORT environment variable or defaults to 8080
+    # Start video scheduler script
+    subprocess.Popen(["python3", "video_scheduler.py"])
+    
+    
+    # Run Flask app (to keep Koyeb service running)
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
